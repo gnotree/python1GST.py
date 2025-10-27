03eadc38fd1c1094e8e5703187a41bf831314aa3
gnotree <120007078+gnotree@users.noreply.github.com>
2025-10-26 23:19:55 -0400

    Create user greeting and password generation script
    The script prompts the user for their first name, last name, and school ID number, then generates a temporary password and displays a greeting.

---
+++ python1GST.py
@@ -0,0 +1,14 @@
+# display a greeting and generate a password
+# get input from the user
+first_name = input("First name: ")
+last_name = input("Last name: ")
+id_number = input("Enter your school ID number: ")
+
+# create strings or else you'll look like this text emoji D:
+name = first_name + " " + last_name
+temp_password = first_name + "*" + id_number
+
+# display the results
+print("Welcome " + name + "!")
+print("Your temporary password is: " + temp_password)
+#Such long code, much complexity, very doge, such elite legendary meme-ary 🐶🐕
